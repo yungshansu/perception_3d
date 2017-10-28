c1539a4a61d39bd46d6f509a2d33a1e86e893242
wuyusin <ehish713.eed04@g2.nctu.edu.tw>
2017-10-28 17:28:11 +0800

    d complete v1

--- d_pcl_surface_normal/src/task1_blank.cpp
+++ d_pcl_surface_normal/src/task1_blank.cpp
@@ -15,7 +15,10 @@
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/segmentation/extract_clusters.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/common/centroid.h>
 #include <queue>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 using namespace pcl;
 using namespace std;
 
@@ -24,6 +27,7 @@ ros::Publisher low_curvature;
 ros::Publisher surface1;
 ros::Publisher surface2;
 ros::Publisher surface3;
+ros::Publisher vis_pub;
 ///The smallest scale to use in the DoN filter.[meter]
 double scale1;
 ///The largest scale to use in the DoN filter.[meter]
@@ -39,89 +43,26 @@ void Callback(const PointCloudXYZRGB::ConstPtr& input)
 	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 	pcl::copyPointCloud (*input,*cloud);
 
-	// Create a search tree, use KDTreee for non-organized data.
-	pcl::search::Search<PointXYZRGB>::Ptr tree;
-	if (cloud->isOrganized ()){
-		tree.reset (new pcl::search::OrganizedNeighbor<PointXYZRGB> ());
-	}
-	else{
-		tree.reset (new pcl::search::KdTree<PointXYZRGB> (false));
-	}
+	/*---------------------------------------
+	something here
+	compute curvature
+	...
 
-	// Set the input pointcloud for the search tree
-	tree->setInputCloud (cloud);
+	referance: ex_node1
 
-	if (scale1 >= scale2){
-		cerr << "Error: Large scale must be > small scale!" << endl;
-		exit (EXIT_FAILURE);
-	}
-
-	// Compute normals using both small and large scales at each point
-	pcl::NormalEstimationOMP<PointXYZRGB, PointNormal> ne;
-	ne.setInputCloud (cloud);
-	ne.setSearchMethod (tree);
-
-	ne.setViewPoint (std::numeric_limits<float>::max (), std::numeric_limits<float>::max (), std::numeric_limits<float>::max ());
-
-	// calculate normals with the small scale
-	cout << "Calculating normals for scale..." << scale1 << endl;
-	pcl::PointCloud<PointNormal>::Ptr normals_small_scale (new pcl::PointCloud<PointNormal>);
-
-	ne.setRadiusSearch (scale1);
-	ne.compute (*normals_small_scale);
-
-	// calculate normals with the large scale
-	cout << "Calculating normals for scale..." << scale2 << endl;
-	pcl::PointCloud<PointNormal>::Ptr normals_large_scale (new pcl::PointCloud<PointNormal>);
-
-	ne.setRadiusSearch (scale2);
-	ne.compute (*normals_large_scale);
-
-	// Create output cloud for DoN results
-	PointCloud<PointXYZRGBNormal>::Ptr doncloud (new pcl::PointCloud<PointXYZRGBNormal>);
-	copyPointCloud<PointXYZRGB, PointXYZRGBNormal>(*cloud, *doncloud);
-
-	cout << "Calculating DoN... " << endl;
-	// Create DoN operator
-	pcl::DifferenceOfNormalsEstimation<PointXYZRGB, PointNormal, PointXYZRGBNormal> don;
-	don.setInputCloud (cloud);
-	don.setNormalScaleLarge (normals_large_scale);
-	don.setNormalScaleSmall (normals_small_scale);
-
-	if (!don.initCompute ())
-	{
-		std::cerr << "Error: Could not intialize DoN feature operator" << std::endl;
-		exit (EXIT_FAILURE);
-	}
-
-	// Compute DoN
-	don.computeFeature (*doncloud);
+	---------------------------------------*/
 
 	// Filter by magnitude
 	cout << "Filtering out DoN mag <= " << threshold << "..." << endl;
+	/*---------------------------------------
+	something here
+	filter the low curvature
+	...
 
-	// Build the condition for filtering
-	pcl::ConditionOr<PointXYZRGBNormal>::Ptr range_cond (
-		new pcl::ConditionOr<PointXYZRGBNormal> ()
-	);
-	//only curvatures which is less than threshold can stay in the pointcloud, LT means less than
-	range_cond->addComparison (pcl::FieldComparison<PointXYZRGBNormal>::ConstPtr (
-		new pcl::FieldComparison<PointXYZRGBNormal> ("curvature", pcl::ComparisonOps::LT, threshold))	 
-	);
-
-	// Build the filter
-	pcl::ConditionalRemoval<PointXYZRGBNormal> condrem (range_cond);
-	condrem.setInputCloud (doncloud);
-
-	pcl::PointCloud<PointXYZRGBNormal>::Ptr doncloud_filtered (new pcl::PointCloud<PointXYZRGBNormal>);
-
-	// Apply filter
-	condrem.filter (*doncloud_filtered);
-
-	doncloud = doncloud_filtered;
+	publish the result
 	low_curvature.publish(*doncloud);
-	 // Filter by magnitude
-	
+	--------------------------------------
+	*/
   	cout << "Clustering using EuclideanClusterExtraction with tolerance <= " << segradius << "..." << endl;
 
   	pcl::search::KdTree<PointXYZRGBNormal>::Ptr segtree (new pcl::search::KdTree<PointXYZRGBNormal>);
@@ -151,26 +92,24 @@ void Callback(const PointCloudXYZRGB::ConstPtr& input)
     	cloud_cluster_don->height = 1;
     	cloud_cluster_don->is_dense = true;
     	cluster_que.push(cloud_cluster_don);
-    	//Save cluster
+    	
     	cout << "PointCloud representing the Cluster: " << cloud_cluster_don->points.size () << " data points." << std::endl;
-    	//stringstream ss;
-    	//ss << "don_cluster_" << j << ".pcd";
-   	 	//writer.write<pcl::PointNormal> (ss.str (), *cloud_cluster_don, false);
+    	
   	}
-	cout<<"cluster num : "<<cluster_que.size()<<endl;
-	//pcl::PointCloud<pcl::PointXYZRGB>::Ptr output (new pcl::PointCloud<pcl::PointXYZRGB>);
-	//copyPointCloud<PointXYZRGBNormal, PointXYZRGB>(*cluster.front(), *output);
-	pcl::SACSegmentation<pcl::PointXYZRGBNormal> seg;
+	
+	
   	pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
   	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
   	queue<pcl::PointCloud<PointXYZRGBNormal>::Ptr> surface_queue;
-  	pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGBNormal> ());
-
+  	
+  	Eigen::Vector4f c[3];
+  	Eigen::Vector3f normal[3];
   	seg.setOptimizeCoefficients (true);
   	seg.setModelType (pcl::SACMODEL_PLANE);
   	seg.setMethodType (pcl::SAC_RANSAC);
   	seg.setMaxIterations (50);
   	seg.setDistanceThreshold (0.01);
+
   	for(int i=0;i<3;i++){
   		seg.setInputCloud (cluster_que.front());
     	seg.segment (*inliers, *coefficients);
@@ -187,21 +126,59 @@ void Callback(const PointCloudXYZRGB::ConstPtr& input)
     	extract.setNegative (false);
 
     	// Get the points associated with the planar surface
+    	pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGBNormal> ());
     	extract.filter(*cloud_plane);
-    	pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr newcloud (new pcl::PointCloud<pcl::PointXYZRGBNormal>);
-		copyPointCloud<PointXYZRGBNormal, PointXYZRGBNormal>(*cloud_plane, *newcloud);
-    	surface_queue.push(newcloud);
+    	/*
+    	somthing here
+    	compute ceter point, surface normal
+    	...
+		store center point in vector c
+		store center point in vector normal
+    	*/
+    	surface_queue.push(cloud_plane);
+    	
     	std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
     	cluster_que.pop();
     }
+    visualization_msgs::MarkerArray markerArray;
+    
+  	for(int i=0;i<3;i++){
+  		visualization_msgs::Marker marker;
+  		marker.type = visualization_msgs::Marker::ARROW;
+  		marker.header.frame_id = doncloud->header.frame_id;
+  		marker.header.stamp = ros::Time::now();
+  		marker.ns = "arrow";
+        marker.id = i;
+        marker.action = visualization_msgs::Marker::ADD;
+        geometry_msgs::Point p;
+      	p.x = c[i][0];
+      	p.y = c[i][1];
+      	p.z = c[i][2];
+     	marker.points.push_back(p);
+
+     	p.x += normal[i][0]/10;
+      	p.y += normal[i][1]/10;
+      	p.z += normal[i][2]/10;
+      	marker.points.push_back(p);
+
+		marker.color.r = 0.0f;
+	    marker.color.g = 1.0f;
+	    marker.color.b = 1.0f;
+		marker.color.a = 1.0;
+		marker.scale.x = 0.01;
+    	marker.scale.y = 0.02;
+    	marker.scale.z = 0.01;
+		markerArray.markers.push_back(marker);
+    }
+    vis_pub.publish(markerArray);
 	surface1.publish(*surface_queue.front());
 	surface_queue.pop();
 	surface2.publish(*surface_queue.front());
 	surface_queue.pop();
 	surface3.publish(*surface_queue.front());
 	surface_queue.pop();
-	//low_curvature.publish(*doncloud);
-	cout<<"Success output\n";
+	
+	cout<<"Success output\n\n";
 }
 
 int main(int argc, char **argv)
@@ -228,7 +205,7 @@ int main(int argc, char **argv)
 	surface1 = n.advertise<PointCloudXYZRGB > ("/camera/task1/surface1", 1);
 	surface2 = n.advertise<PointCloudXYZRGB > ("/camera/task1/surface2", 1);
 	surface3 = n.advertise<PointCloudXYZRGB > ("/camera/task1/surface3", 1);
-	ros::spin();
-
+	vis_pub = n.advertise<visualization_msgs::MarkerArray>( "center_point_normal", 0);
+	ros::spin ();
 	return 0;
 }
